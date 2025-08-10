d9e55c03d774e31566fbe4271b4436df4c9976e3
Aastha-Bharti <asbharti27@gmail.com>
2025-08-11 03:01:16 +0530

    new

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,23 +1,47 @@
+// export const BASE_URL = import.meta.env.VITE_API_URL;
+
+// //ROUTES USED FOR FRONTEND
+// export const API_PATHS = {
+//     AUTH: {
+//         REGISTER : '/api/auth/register',
+//         LOGIN : '/api/auth/login',
+//         GET_PROFILE : '/api/auth/profile'
+//     }, 
+//     RESUME : {
+//         CREATE : '/api/resume',
+//         GET_ALL : '/api/resume',
+//         GET_BY_ID : (id) => `/api/resume/${id}`,
+
+//         UPDATE : (id) => `/api/resume/${id}`,
+//         DELETE : (id) => `/api/resume/${id}`,
+
+//         UPLOAD_IMAGES : (id) => `/api/resume/${id}/upload-images`
+//     },
+//     image : {
+//         UPLOAD_IMAGE : '/api/auth/upload-image'
+//     }
+// }
+
 export const BASE_URL = import.meta.env.VITE_API_URL;
 
 //ROUTES USED FOR FRONTEND
 export const API_PATHS = {
     AUTH: {
-        REGISTER : '/api/auth/register',
-        LOGIN : '/api/auth/login',
-        GET_PROFILE : '/api/auth/profile'
+        REGISTER : '/auth/register',
+        LOGIN : '/auth/login',
+        GET_PROFILE : '/auth/profile'
     }, 
     RESUME : {
-        CREATE : '/api/resume',
-        GET_ALL : '/api/resume',
-        GET_BY_ID : (id) => `/api/resume/${id}`,
+        CREATE : '/resume',
+        GET_ALL : '/resume',
+        GET_BY_ID : (id) => `/resume/${id}`,
 
-        UPDATE : (id) => `/api/resume/${id}`,
-        DELETE : (id) => `/api/resume/${id}`,
+        UPDATE : (id) => `/resume/${id}`,
+        DELETE : (id) => `/resume/${id}`,
 
-        UPLOAD_IMAGES : (id) => `/api/resume/${id}/upload-images`
+        UPLOAD_IMAGES : (id) => `/resume/${id}/upload-images`
     },
     image : {
-        UPLOAD_IMAGE : '/api/auth/upload-image'
+        UPLOAD_IMAGE : '/auth/upload-image'
     }
 }
